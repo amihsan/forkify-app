f273521169afeeaea6eae3e598b3ee6482d871ba
amihsan <rumi.apece.du@gmail.com>
2022-05-25 03:45:09 +0200

    fracty used instead of Fractionale

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '6f5bf644-40ec-4cb3-bc38-0711424909d9';
+export const KEY = '<6f5bf644-40ec-4cb3-bc38-0711424909d9>';
 export const MODAL_CLOSE_SEC = 2.5;
